b718abe67f4f4ff1aa71562eadcf8ef18106b1e6
Mohammad Tahmasebi <mohammad.tahmasebi@students.codeinstitute.net>
2024-10-04 17:47:23 +0000

    answer to filter challenge

--- filter.js
+++ filter.js
@@ -4,7 +4,7 @@
  * command node filter.js in the terminal
  */
 
-
+/*
 // Simple Filtering
 const people = [
   {
@@ -21,6 +21,11 @@ const people = [
   },
 ];
 
+const oldEnough = people.filter(person => person.age >= 21);
+console.log(oldEnough);
+
+const paul = people.filter(p => p.name === 'Paul');
+console.log(paul);
 
 // Complex Filtering
 const students = [
@@ -55,3 +60,39 @@ const students = [
     ]
   },
 ];
+
+const has5yrsExp = skill => skill.yrsExperience >= 5;
+const hasStrongSkills = student => student.skills.filter(has5yrsExp).length > 0;
+const candidates = students.filter(hasStrongSkills);
+console.log(candidates.map(c => c.name));
+*/
+
+let students = [
+  {
+      name: 'John',
+      subjects: ['maths', 'english', 'cad'],
+      teacher: {maths: 'Harry', english: 'Joan', cad: 'Paul'},
+      results: {maths: 90, english: 75, cad: 87},
+  },
+  {
+      name: 'Emily',
+      subjects: ['science', 'english', 'art'],
+      teacher: {science: 'Iris', english: 'Joan', art: 'Simon'},
+      results: {science: 93, english: 80, art: 95},
+  },
+  {
+      name: 'Adam',
+      subjects: ['science', 'maths', 'art'],
+      teacher: {science: 'Iris', maths: 'Harry', art: 'Simon'},
+      results: {science: 84, maths: 97, art: 95},
+  },
+  {
+      name: 'Fran',
+      subjects: ['science', 'english', 'art'],
+      teacher: {science: 'Iris', english: 'Joan', art: 'Simon'},
+      results: {science: 67, english: 87, art: 95},
+  }
+];
+
+const topMaths = students.filter(s => s.results['maths'] >= 90);
+console.log(topMaths);
